5c2614158830f9b94d24b69d215536dfd96cffef
Licio Lentimo <lentimo@gmail.com>
2018-09-30 20:32:49 +0300

    confirmation

---
+++ confirmation/index3.php
@@ -0,0 +1,8 @@
+<?php
+$data = file_get_contents('php://input');
+
+$handle = fopen('confirmation.txt', 'w');
+
+fwrite($handle, $data);
+
+?>
--- validation/index2.php
+++ validation/index2.php
@@ -1,8 +1,23 @@
 <?php
 $data = file_get_contents('php://input');
 
-$handle = fopen('validation.txt', 'w');
+//$handle = fopen('validation.txt', 'w');
 
-fwrite($handle, $data);
+//fwrite($handle, $data);
+
+$json_decode = json_decode($data);
+$amount = $data['TransAmount'];
+
+if($amount > 100) {
+    $response = array(
+        "ResultCode"=>0,
+        "ResultDesc"=>"Accepted"
+    );
+    $json_response = json_encode($response);
+
+    header('Content-type: application/json');
+
+    echo $json_response;
+}
 
 ?>
